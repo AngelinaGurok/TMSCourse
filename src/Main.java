8a2cc43d19604b1686a28d080388cd7964f30b16
Angelina <a.hurok@gmail.com>
2023-04-24 15:48:51 +0300

    added time calculator (seconds)

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,15 @@
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
 
+/**
+ * Тривиальная (3 балла).
+ *
+ * Задача имеет повышенную стоимость как первая в списке.
+ *
+ * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
+ * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
+ */
+
 import java.util.Scanner;
 public class Main {
 
@@ -50,26 +59,33 @@ public class Main {
         double numb = in.nextDouble();
         System.out.println(" The square of number " + numb + " is equal to " + sqrtDouble(numb));*/
 
-/*        System.out.println("Input train departure time in hours and minutes: ");
+        /*        System.out.println("Input train departure time in hours and minutes: ");
         int hoursDep = in.nextInt();
         int minutesDep = in.nextInt();
         System.out.println("Input train arrival time in hours and minutes: ");
         int hoursArr = in.nextInt();
         int minutesArr = in.nextInt();
         System.out.println(" Travel time in minutes is " + travelTime(hoursDep, minutesDep, hoursArr, minutesArr));*/
-        System.out.println("Input number: ");
-        int numb = in.nextInt();
-        System.out.println("The third number of " + numb + " is: " + thirdDigit(numb));
+
+        System.out.println("Input time inhours, minutes and seconds: ");
+        int hours = in.nextInt();
+        int minutes = in.nextInt();
+        int seconds = in.nextInt();
+        System.out.println(timeCalc(hours, minutes, seconds) + " second have passed since the beginning of the day");
     }
 
-    public static int thirdDigit (int a) {
+    public static int timeCalc (int h, int m, int s){
+        return (h * 60 * 60 + m * 60 + s);
+    }
+
+   /* public static int thirdDigit (int a) {
         int result = a;
         do{
             result = result / 10;
         }while(result > 100);
         result = result % 10;
         return result;
-    }
+    }*/
 
     /*public static int travelTime (int hDep, int mDep, int hArr, int mArr){
         return (mArr + (60 - mDep) + (hArr - hDep - 1) * 60);
